3e07acaa06f0e3b35d82a9aa425a4c33e81b243d
ptidke9 <ptidke9@gmail.com>
2020-07-11 21:06:33 +0530

    payment display added

--- app/src/main/java/com/example/expensesrecordapp/ThirdFrag.java
+++ app/src/main/java/com/example/expensesrecordapp/ThirdFrag.java
@@ -1,5 +1,7 @@
 package com.example.expensesrecordapp;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -9,6 +11,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -21,13 +24,17 @@ import com.example.expensesrecordapp.ui.main.SupplierAdapter;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static com.example.expensesrecordapp.R.layout.list_view;
 
@@ -41,6 +48,7 @@ public class ThirdFrag extends Fragment {
     private List<String> lv = new ArrayList<>();
     private CollectionReference works = db.collection("Works");
     ListView lvSuppliers;
+    SharedPreferences sharedpreferences;
 
     View view;
 
@@ -49,18 +57,47 @@ public class ThirdFrag extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_third, container, false);
         lvSuppliers = view.findViewById( R.id.LvSuppliers );
+        sharedpreferences = view.getContext().getSharedPreferences( "payments", Context.MODE_PRIVATE );
         setList();
 
         lvSuppliers.setOnItemClickListener( new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 String nameS = parent.getItemAtPosition( position ).toString();
+                SharedPreferences.Editor editor = sharedpreferences.edit();
                 BottomSheetDialog mBottomSheetDialog = new BottomSheetDialog(parent.getContext());
                 View dialogView = getActivity().getLayoutInflater().inflate(R.layout.bottom_dailog, null);
                 RecyclerView suppliersRecyclerView = dialogView.findViewById(R.id.list1);
                 TextView tv = dialogView.findViewById(R.id.tv1);
                 tv.setText(toTitleCase( nameS ));
+                TextView tvPaidTotal = dialogView.findViewById( R.id.paidTotal );
                 TextView gt = dialogView.findViewById(R.id.grandTotal1);
+                TextInputEditText paidAmount = dialogView.findViewById( R.id.paidAmount );
+                MaterialButton pay = dialogView.findViewById( R.id.addPayment );
+                tvPaidTotal.setText( "Paid Amount : "  + sharedpreferences.getFloat( nameS, 0 ));
+                pay.setOnClickListener( new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        try {
+
+                            if(Float.valueOf( paidAmount.getText().toString() ) != 0.0) {
+                                tvPaidTotal.setText( "Paid Amount : " + paidAmount.getText().toString() );
+                                editor.putFloat( nameS, Float.valueOf( paidAmount.getText().toString() ) );
+                                editor.commit();
+                                Toast.makeText( dialogView.getContext(), "Payment Updated", Toast.LENGTH_SHORT ).show();
+                            }
+                            else {
+                                Toast.makeText( dialogView.getContext(), "Enter amount higher than 0!", Toast.LENGTH_SHORT ).show();
+                            }
+
+                            paidAmount.setText( "" );
+
+                        }
+                        catch (Exception e){
+                            Toast.makeText( dialogView.getContext(), e.getMessage(), Toast.LENGTH_SHORT ).show();
+                        }
+                    }
+                } );
                 materialsList.clear();
 
                 worksRef.get().addOnCompleteListener( new OnCompleteListener<QuerySnapshot>() {
